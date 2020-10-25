84e049931ae427a12c082b3fe55aadd5b48c3862
Jeonwoo <vasci@kakao.com>
2020-10-25 20:30:07 +0900

    sass, layout edit

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
     try{
         const users = await User.find({});
+        console.log('users 이거는!');
+        console.log({ users });
         res.render('mongoose', { users });
     } catch (err) {
         console.error(err);
