533e97667145b8cc1a81d36f25a947cb161639bd
Jeonwoo <vasci@kakao.com>
2020-11-02 21:17:36 +0900

    edit minor part

--- routes/itemlists.js
+++ routes/itemlists.js
@@ -6,11 +6,8 @@ const router = express.Router();
 router.route('/')
 .get(async (req, res, next) => {
     try{
-        console.log('왔냐?');
-        const itemlists = await Itemlist.find({});
-        //console.log(itemlists);
-        // res.json(itemlists);
-        res.render('itemlist', { itemlists });
+        const items = await Itemlist.find({});
+        res.json(items);
     } catch(err) {
         console.error(err);
         next(err);
@@ -18,10 +15,9 @@ router.route('/')
 });
 
 //이건 조건별 아이템리스트 불러올때로 수정해서 쓸 수 있을듯?
-router.get('/:cond', async(req, res, next) => {
+router.get(`/:cond`, async(req, res, next) => {
     try {
         const items = await Itemlist.find({ 'desc': `/${req.params.condition}/i` });
-        console.log('오나',items);
         res.json(items);
     } catch (err) {
         console.error(err);
